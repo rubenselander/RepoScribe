abfaa8d0be61d8d17a9f17f031f625da59ca2736
Ruben Selander <77073569+Reversehobo@users.noreply.github.com>
2024-02-01 04:07:59 +0100

    Update version and Python requirement in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="RepoScribe",
-    version="0.1.6",
+    version="0.2.0",
     author="Ruben Selander",
     author_email="info@nordicintel.com",
     description="A lightweight tool to compile project files and directories into a structured Markdown document",
@@ -17,8 +17,8 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
     ],
-    python_requires=">=3.8",  # Change as appropriate
-    install_requires=open("requirements.txt").read().splitlines(),
+    python_requires=">=3.10",  # Change as appropriate
+    # install_requires=open("requirements.txt").read().splitlines(),
     entry_points={
         "console_scripts": [
             "reposcribe=reposcribe.cli:main",  # Assuming 'cli.py' contains the CLI interface
